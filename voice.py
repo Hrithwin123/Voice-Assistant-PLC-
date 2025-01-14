ee3a852ed035efeffea9e6d46cf8df00b835ce82
Hrithwin <hrithwin123@gmail.com>
2025-01-14 20:03:40 +0530

    open tabs, msges, searches

---
+++ voice.py
@@ -0,0 +1,115 @@
+import speech_recognition as sr
+import pyttsx3 as ttsx
+import webbrowser as wb
+import sys
+import pywhatkit as whatsapp
+
+
+from contacts import contactlist
+
+recognizer = sr.Recognizer()
+tts = ttsx.init()
+
+def speak(text):
+    tts.say(text)
+    tts.runAndWait()
+
+def commandlist(command):
+    if("google" in command.lower()):
+        wb.open("https://www.google.com/")
+        speak("opening google")
+
+    elif("gmail" in command.lower()):
+        wb.open("https://www.gmail.com/")
+        speak("opening gmail")
+    
+    elif("chat gpt" in command.lower()):
+        wb.open("https://www.chatgpt.com/")
+        speak("opening chat gpt")
+    
+    elif("whatsapp" in command.lower()):
+        wb.open("https://web.whatsapp.com/")
+        speak("opening whatsapp")
+
+    elif("youtube" in command.lower()):
+        wb.open("https://youtube.com/")
+        speak("opening youtube")
+
+    elif("message" in command.lower()):
+        message = []
+        check = False
+        wordlist = command.split()
+        for word in wordlist:
+
+            if(check):
+                message.append(word)
+            if(word == "message"):
+                check = True
+
+        person = message[0]
+        message = " ".join(message[1:])
+
+
+        print(person)
+        if(person.lower() in contactlist):
+            print(contactlist[person.lower()])
+            print(message)    
+            whatsapp.sendwhatmsg_instantly(contactlist[person.lower()], message,7,True,1)
+        else:
+            speak("Contact not found")
+            print("Contact not found")
+
+
+    elif("search" in command.lower()):
+        search = []
+        check = False
+        wordlist = command.split()
+        for word in wordlist:
+            if(check):
+                search.append(word)
+            if(word == "search"):
+                check = True
+        search = " ".join(search)
+        whatsapp.search(search)
+
+    else:
+        print("No command available")
+            
+def initialListen():
+    with sr.Microphone() as source:
+        while True:
+            try:
+                print("Listening....")
+                audio = recognizer.listen(source, timeout = 1)
+                speech = recognizer.recognize_google(audio)
+
+                if("python" in speech.lower()):
+                    break
+
+                elif("exit" in speech.lower()):
+                    speak("Python Shutting Down")
+                    sys.exit(0)
+                    
+            except:
+                print("Word wasnt recognisable")
+
+        print("What would you like to do sir ??")
+        speak("What would you like to do sir ??")
+        commandlisten()
+
+def commandlisten():
+    with sr.Microphone() as source:
+        while True:
+            try : 
+                command_speech = recognizer.listen(source)
+                command = recognizer.recognize_google(command_speech)
+                if("stop" in command.lower()):
+                    speak("Python has stopped listening for commands")
+                    initialListen()
+                else:
+                    commandlist(command)
+
+            except : 
+                print("Command wasnt recognisable")
+
+initialListen()
